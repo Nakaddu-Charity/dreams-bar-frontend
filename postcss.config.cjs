2caea4c4ecbe3f633bab4de9b34b2c81619affaf
Nakaddu-Charity <nakaddu30@gmail.com>
2025-07-20 18:33:25 +0300

    Fix Tailwind content configuration in postcss.config.cjs and revert build script

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,21 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      config: {
+        content: [
+          "./index.html",
+          "./src/**/*.{js,jsx,ts,tsx}",
+        ],
+        theme: {
+          extend: {
+            fontFamily: {
+              sans: ['Inter', 'sans-serif'],
+            },
+          },
+        },
+        plugins: [],
+      }
+    },
     autoprefixer: {},
   },
 };
-
