10f950ef6f337d4b2036eabed72ea969bb90e8f5
Nakaddu-Charity <nakaddu30@gmail.com>
2025-07-20 17:16:53 +0300

    Use postcss.config.cjs and add postinstall script for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Remove the css.postcss block here, as it will now rely on postcss.config.js
+  // No css.postcss block here
 });
