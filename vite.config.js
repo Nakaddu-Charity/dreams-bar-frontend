29d90fdbebe1bd0bdedb743b22a31f009d295531
Nakaddu-Charity <nakaddu30@gmail.com>
2025-07-20 16:54:33 +0300

    Explicitly define PostCSS plugins in vite.config.js for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'), // Use require for CommonJS context in Vite config
+        require('autoprefixer'), // Use require for CommonJS context in Vite config
+      ],
+    },
+  },
+});
